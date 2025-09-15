cb8232b28f5adce80a5713806fb5000856f8644d
gabriel buhendwa <gabrielbuhendwa400@gmail.com>
2025-09-15 13:00:32 +0100

    add this to my repository

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,16 +1,16 @@
 const defaultResult = 0;
-
 let currentResult = defaultResult;
 
-currentResult = (currentResult + 10) * 3 / 2 - 1;
+function add(num1, num2){
+    const result = num1 + num2;
+    return result;
+}
+
+currentResult = add(200, 8);
 
 let calculationDescription = `(${defaultResult} + 10) * 3 / 2 - 1`;
 
 outputResult(currentResult, calculationDescription);
 
-function add(num1, num2){
-    const result = num1 + num2;
-    alert('the result is' + result);
-}
-add(200, 8);
+
 
