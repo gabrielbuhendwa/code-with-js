79b54c5df3198adec8737cfa4bc0f68717bf8964
gabriel buhendwa <gabrielbuhendwa400@gmail.com>
2025-09-15 22:35:21 +0100

    add this to my repository

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,19 +1,44 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
 
-function getUserInput(){
-    return parseInt(userInput.value);
+function getUserNumberInput() {
+  return parseInt(usrInput.value);
 }
 
-function add(){
-    const enteredNumber = getUserInput;
-    const calcDescription = `${currentResult} + ${enteredNumber}`
-    currentResult = currentResult + enteredNumber;
-    outputResult(currentResult, calcDescription);
+function createAndWriteOutput(operator, resultBeforeCalc, calcNumber) {
+  const calcDescription = `${resultBeforeCalc} ${operator} ${calcNumber}`;
+  outputResult(currentResult, calcDescription);
 }
-addBtn.addEventListener('click', add);  
 
+function add() {
+  const enteredNumber = getUserNumberInput();
+  const initialResult = currentResult;
+  currentResult = currentResult + enteredNumber;
+  createAndWriteOutput('+', initialResult, enteredNumber);
+}
 
+function subtract() {
+  const enteredNumber = getUserNumberInput();
+  const initialResult = currentResult;
+  currentResult = currentResult - enteredNumber;
+  createAndWriteOutput('-', initialResult, enteredNumber);
+}
 
+function multiply() {
+  const enteredNumber = getUserNumberInput();
+  const initialResult = currentResult;
+  currentResult = currentResult * enteredNumber;
+  createAndWriteOutput('*', initialResult, enteredNumber);
+}
 
+function divide() {
+  const enteredNumber = getUserNumberInput();
+  const initialResult = currentResult;
+  currentResult = currentResult / enteredNumber;
+  createAndWriteOutput('/', initialResult, enteredNumber);
+}
 
+addBtn.addEventListener('click', add);
+subtractBtn.addEventListener('click', subtract);
+multiplyBtn.addEventListener('click', multiply);
+divideBtn.addEventListener('click', divide);
