f7897f2c430cd079c38eeb9669a9ad8dc7ce1489
gabriel buhendwa <gabrielbuhendwa400@gmail.com>
2025-09-18 20:23:14 +0100

    add this to my repository

--- Monster killer game/assets/scripts/app.js
+++ Monster killer game/assets/scripts/app.js
@@ -2,6 +2,7 @@
 const ATTACK_VALUE = 10;
 const STRONG_ATTACK_VALUE = 17;
 const MONSTER_ATTACK_VALUE = 14;
+const HEAL_VALUE = 20;
 
 //variables to hold max life and current life of player and monster
 let chosenMaxLife = 100;
@@ -9,7 +10,21 @@ let currentMonsterHealth = chosenMaxLife;
 let currentPlayerHealth = chosenMaxLife;
 
 //get the attack button from the DOM
-adjustHealthBars(chosenMaxLife); 
+adjustHealthBars(chosenMaxLife);
+
+//this fonction will handle the end round logic for player and monster
+function endRound(){
+    //deal damage to player 
+    const playerDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE);
+    currentPlayerHealth -= playerDamage;
+    if (currentMonsterHealth <= 0 && currentPlayerHealth > 0){
+        alert("congrats you won");
+    }else if (currentPlayerHealth <=0 && currentMonsterHealth > 0){
+        alert("Oh, saddly you lost");
+    }else if(currentPlayerHealth <= 0 && currentMonsterHealth <= 0){
+        alert("this case is a draw");
+    }
+}
 
 //function to handle attack and strong attack logic for player and monster
 function attackMonster(mode){
@@ -22,15 +37,7 @@ function attackMonster(mode){
     //deal damage to monster and player
     const damage = dealMonsterDamage(maxDamage);
     currentMonsterHealth -= damage; 
-    const playerDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE);
-    currentPlayerHealth -= playerDamage;
-    if (currentMonsterHealth <= 0 && currentPlayerHealth > 0){
-        alert("congrats you won");
-    }else if (currentPlayerHealth <=0 && currentMonsterHealth > 0){
-        alert("Oh, saddly you lost");
-    }else if(currentPlayerHealth <= 0 && currentMonsterHealth <= 0){
-        alert("this case is a draw");
-    }
+    endRound();
 }
 //fuction to handle attack button click for player and monster
 function attackHandler(){
@@ -43,7 +50,15 @@ function strongAttackHandler(){
 
 //function to handle heal button click for player
 function healPlayerHandler(){
-
+    let healValue;
+    if(currentPlayerHealth >= chosenMaxLife - HEAL_VALUE)//basically this means if the current health of player is greater than or equal to max life - heal value that means we cant heal the player to more than his max initial health
+    {
+        alert("you can't heal to more than your max initial health");
+        healValue = chosenMaxLife - currentPlayerHealth;
+    }
+    increasePlayerHealth(HEAL_VALUE);
+    currentPlayerHealth += HEAL_VALUE;
+    endRound();
 }
 
 //add event listener to attack button and strong attack button
