153e46b5998e8575f665dec1ea246888175733d0
gabriel buhendwa <gabrielbuhendwa400@gmail.com>
2025-09-18 16:36:55 +0100

    add this to my repository

--- Monster killer game/assets/scripts/app.js
+++ Monster killer game/assets/scripts/app.js
@@ -1,15 +1,20 @@
+//constant that holds the attack value
 const ATTACK_VALUE = 10;
 
+//variables to hold max life and current life of player and monster
 let chosenMaxLife = 100;
 let currentMonsterHealth = chosenMaxLife;
 let currentPlayerHealth = chosenMaxLife;
 
+//get the attack button from the DOM
 adjustHealthBars(chosenMaxLife); 
 
+//fuction to handle attack button click
 function attackHandler(){
     const damage = dealMonsterDamage(ATTACK_VALUE);
     currentMonsterHealth -= damage; 
 }
 
+//add event listener to attack button
 attackBtn.addEventListener('click', attackHandler);
 
