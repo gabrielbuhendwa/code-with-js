6ff24238cb5a33592b2bc5ebbab33c1cea1f0bc9
gabriel buhendwa <gabrielbuhendwa400@gmail.com>
2025-09-22 16:43:26 +0100

    add this to my repository

--- 3. Behind JS Scenes, practice/app.js
+++ 3. Behind JS Scenes, practice/app.js
@@ -1,7 +1,9 @@
+//this block of code here defines a function called getName, which basically prompts the user to enter their name and returns it
 function getName(){
     return prompt('your name is', '');
 }
 
+//this function here contains a call to getName(), which is also defined in this file
 function greet(){
     let userName = getName();
     console.log('hello ' + userName);
