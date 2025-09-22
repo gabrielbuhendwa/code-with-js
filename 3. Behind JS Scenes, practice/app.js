90c0add3eea1caa1857eb4edc03e0646e9145d78
gabriel buhendwa <gabrielbuhendwa400@gmail.com>
2025-09-22 15:03:49 +0100

    add this to my repository

---
+++ 3. Behind JS Scenes, practice/app.js
@@ -0,0 +1,7 @@
+function userInfos() {
+    let userName = prompt('enter your name', '');
+    let userAge = prompt('enter your age', '');
+    alert(`Your name is ${userName} and you are ${userAge} years old`);
+}
+
+userInfos();
