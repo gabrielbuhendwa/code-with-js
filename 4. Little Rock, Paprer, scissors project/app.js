8514eeac62830410ff3cc5384a04a0c31fdea07b
gabriel buhendwa <gabrielbuhendwa400@gmail.com>
2025-09-23 21:34:39 +0100

    add this to my repository

--- 4. Little Rock, Paprer, scissors project/app.js
+++ 4. Little Rock, Paprer, scissors project/app.js
@@ -6,6 +6,8 @@ const PAPER = 'PAPER';
 const SCISSORS = 'SCISSORS';
 const DEFAULT_USER_CHOICE = ROCK;
 
+let gameIsRunning = false;
+
 
 const getPlayerChoice = function(){
   const selection = prompt(`${ROCK}, ${PAPER}, ${SCISSORS}`, '').toUpperCase();
@@ -23,7 +25,11 @@ const getPlayerChoice = function(){
 }
 
 startGameBtn.addEventListener('click', function() {
+  if(gameIsRunning){
+    return;
+  }
+  gameIsRunning = true;
   console.log('Game is starting...');
-  const playerChoice = getPlayerChoice();
-  console.log('Player choice:', playerChoice);
+  const playerSelection = getPlayerChoice();
+  console.log(playerSelection);
 });
