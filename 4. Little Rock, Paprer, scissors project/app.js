9a9e076070bb159daf53a415beb59d82cc610dea
gabriel buhendwa <gabrielbuhendwa400@gmail.com>
2025-09-23 20:51:24 +0100

    add this to my repository

--- 4. Little Rock, Paprer, scissors project/app.js
+++ 4. Little Rock, Paprer, scissors project/app.js
@@ -1,22 +1,26 @@
 const startGameBtn = document.getElementById('start-game-btn');
 
-// const start = function() {
-//   console.log('Game is starting...');
-// };
-
-// const person = {
-//   name: 'Max',
-//   greet: function greet() {
-//     console.log('Hello there!');
-//   }
-// };
-
-// person.greet();
-
-// console.dir(startGame);
+//creating constant for different choices 
+const ROCK = 'ROCK';
+const PAPER = 'PAPER';
+const SCISSORS = 'SCISSORS';
+const DEFAULT_USER_CHOICE = ROCK;
 
 
-//here we used what we called anonymous function that means that we don't have a name for the function
+const getPlayerChoice = function(){
+  const selection = prompt(`${ROCK}, ${PAPER}, ${SCISSORS}`, '').toUpperCase();
+
+  //using conditions to avoid user to enter anything he want 
+  if(
+    selection !== ROCK &&
+    selection !== PAPER && 
+    selection !== SCISSORS
+  ) {
+    alert(`Invalid choice! we chose ${ROCK} for you`);
+  }
+
+}
+
 startGameBtn.addEventListener('click', function() {
   console.log('Game is starting...');
 });
