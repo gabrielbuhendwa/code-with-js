cfce584af824c5cba7f0fb55e3d3f380ca6f8104
gabriel buhendwa <gabrielbuhendwa400@gmail.com>
2025-09-23 13:44:55 +0100

    add this to my repository

--- 4. Little Rock, Paprer, scissors project/app.js
+++ 4. Little Rock, Paprer, scissors project/app.js
@@ -4,15 +4,6 @@ const start = function() {
   console.log('Game is starting...');
 };
 
-// const person = {
-//   name: 'Max',
-//   greet: function greet() {
-//     console.log('Hello there!');
-//   }
-// };
 
-// person.greet();
-
-// console.dir(startGame);
 
 startGameBtn.addEventListener('click', start);
