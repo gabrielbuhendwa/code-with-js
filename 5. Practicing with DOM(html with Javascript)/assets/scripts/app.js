ca249f950eb488f76e8dd760ce0f072683916691
gabriel buhendwa <gabrielbuhendwa400@gmail.com>
2025-09-26 00:11:33 +1245

    add this to my repository

---
+++ 5. Practicing with DOM(html with Javascript)/assets/scripts/app.js
@@ -0,0 +1,14 @@
+//selecting the add modal 
+const addMovieModal = document.getElementById('add-modal');
+
+//selecting the first-add button
+const startAddMovieButton = document.querySelector('header button');
+
+//creating function for the event Listener 
+const toggleMovieModal = () => {
+    //here we are toggling the class visible in order to show and hide the modal
+     
+}  
+
+//adding an event Listener to the buttom in order to manupilate it 
+startAddMovieButton.addEventListener('click', toggleMovieModal )
