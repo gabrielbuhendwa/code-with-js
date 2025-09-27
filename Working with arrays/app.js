930acaa36e8a6fcde017c79aa31c54307e3baa6f
gabriel buhendwa <gabrielbuhendwa400@gmail.com>
2025-09-27 22:26:44 +1245

    add this to my repository

--- Working with arrays/app.js
+++ Working with arrays/app.js
@@ -1,1 +1,12 @@
+const number = [1, 2, 3];
 
+console.log('number:', number);
+
+//other ways of using arrays 
+const analyticsData = [[1,2,2], [3,4,5]];
+
+for (const data of analyticsData){
+    for(const datapoint of data){
+        console.log(datapoint);
+    }
+}
