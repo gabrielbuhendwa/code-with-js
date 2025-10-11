362369be5386e20c8d093629b8387ed1c1c5ff68
gabriel buhendwa <gabrielbuhendwa400@gmail.com>
2025-10-12 02:31:46 +1345

    add this to my repository

--- 12. More on events/assets/scripts/events.js
+++ 12. More on events/assets/scripts/events.js
@@ -1,4 +1,4 @@
-const buttons = document.querySelectorAll('button');
+const button = document.querySelector('button');
 
 // button.onclick = function() {
 
@@ -24,10 +24,30 @@ const boundFn = buttonClickHandler.bind(this);
 //   button.removeEventListener('click', buttonClickHandler);
 // }, 2000);
 
-buttons.forEach(btn => {
-  btn.addEventListener('mouseenter', buttonClickHandler);
+// buttons.forEach(btn => {
+//   btn.addEventListener('mouseenter', buttonClickHandler);
+// });
+
+// window.addEventListener('scroll', event => {
+//   console.log(event);
+// });
+
+const form = document.querySelector('form');
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+  console.log(event);
 });
 
-window.addEventListener('scroll', event => {
+const div = document.querySelector('div');
+
+div.addEventListener('mouseenter', event => {
+  console.log('CLICKED DIV');
+  console.log(event);
+});
+
+button.addEventListener('mouseenter', event => {
+  event.stopPropagation();
+  console.log('CLICKED BUTTON');
   console.log(event);
 });
