af19772b24cf735dec3303b7b579575b4e54149e
gabriel buhendwa <gabrielbuhendwa400@gmail.com>
2025-10-14 20:25:58 +1345

    add this to my repository

--- 14. Async and Callbacks in JS/app.js
+++ 14. Async and Callbacks in JS/app.js
@@ -1,20 +1,41 @@
 const button = document.querySelector('button');
 const output = document.querySelector('p');
 
+const getPosition = opts => {
+  const promise = new Promise((resolve, reject) => {
+    navigator.geolocation.getCurrentPosition(
+      success => {
+        resolve(success);
+      },
+      error => {},
+      opts
+    );
+  });
+  return promise;
+};
+
+const setTimer = duration => {
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve('Done!');
+    }, duration);
+  });
+  return promise;
+};
+
 function trackUserHandler() {
-  navigator.geolocation.getCurrentPosition(
-    posData => {
-      setTimeout(() => {
-        console.log(posData);
-      }, 2000);
-    },
-    error => {
-      console.log(error);
-    }
-  );
-  setTimeout(() => {
+  let positionData;
+  getPosition()
+    .then(posData => {
+      positionData = posData;
+      return setTimer(2000);
+    })
+    .then(data => {
+      console.log(data, positionData);
+    });
+  setTimer(1000).then(() => {
     console.log('Timer done!');
-  }, 0);
+  });
   console.log('Getting position...');
 }
 
